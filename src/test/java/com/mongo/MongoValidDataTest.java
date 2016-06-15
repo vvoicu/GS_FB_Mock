eab3a80c1cc0eaccbf2b69f983bd1b128f726c3a
calinmarchis <calinmarchis@EVO263.evointern>
2016-06-15 11:28:27 +0300

    Updated MongoValidDataTest

--- src/test/java/com/mongo/MongoValidDataTest.java
+++ src/test/java/com/mongo/MongoValidDataTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import com.connector.MongoConnector;
 import com.connector.MongoWrite;
+import com.data.model.messages.DataBasicModel;
 import com.data.model.messages.MessageObjectModel;
 
 public class MongoValidDataTest {
@@ -14,6 +15,11 @@ public class MongoValidDataTest {
 		MessageObjectModel messageObjectModel;
 		for (int i = 0; i < 10; i++) {
 			messageObjectModel = new MessageObjectModel();
+			
+//			messageObjectModel.messages.data.to.data.add(new DataBasicModel());
+////			messageObjectModel.messages.data.to.data.add(new DataBasicModel().setEmail("myEmail@nothing"));
+//			messageObjectModel.messages.data.to.data.add(new DataBasicModel());
+			
 			MongoWrite.writePackageObjectModel(messageObjectModel);
 		}
 	}
